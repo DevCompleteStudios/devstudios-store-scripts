60492356d96600eabab31b2861e5ac6a9578e5bc
DevCompleteStudios <jorge.yael333@gmail.com>
2024-10-17 13:08:16 -0600

    first commit

---
+++ src/main/java/com/devstudios/dbu/devstudios_dbu/DevstudiosDbuApplication.java
@@ -0,0 +1,13 @@
+package com.devstudios.dbu.devstudios_dbu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevstudiosDbuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevstudiosDbuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devstudios/dbu/devstudios_dbu/DevstudiosDbuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devstudios.dbu.devstudios_dbu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevstudiosDbuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
